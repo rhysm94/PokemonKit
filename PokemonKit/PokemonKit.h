88d50c28578e8531ba04aa97aa4b4bfd012830ee
Rhys Morgan <rhysm@me.com>
2018-02-28 15:49:15 +0000

    Initial Commit

---
+++ PokemonKit/PokemonKit.h
@@ -0,0 +1,19 @@
+//
+//  PokemonKit.h
+//  PokemonKit
+//
+//  Created by Rhys Morgan on 28/02/2018.
+//  Copyright © 2018 Rhys Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PokemonKit.
+FOUNDATION_EXPORT double PokemonKitVersionNumber;
+
+//! Project version string for PokemonKit.
+FOUNDATION_EXPORT const unsigned char PokemonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokemonKit/PublicHeader.h>
+
+
