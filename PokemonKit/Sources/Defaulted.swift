fa61b2362652e8c5f73ca039d7b1960edbae79df
Rhys Morgan <rhysm@me.com>
2020-05-17 11:04:47 +0100

    Added Defaulted propertyWrapper for nicknames

---
+++ PokemonKit/Sources/Defaulted.swift
@@ -0,0 +1,26 @@
+//
+//  Defaulted.swift
+//  PokemonKit-iOS
+//
+//  Created by Rhys Morgan on 17/05/2020.
+//  Copyright © 2020 Rhys Morgan. All rights reserved.
+//
+
+@propertyWrapper
+public struct Defaulted<T> {
+	let defaultValue: T
+
+	private	var value: T?
+
+	init(defaultValue: T, value: T?) {
+		self.defaultValue = defaultValue
+		self.value = value
+	}
+
+	public var wrappedValue: T {
+		get { value ?? defaultValue }
+		set { value = newValue }
+	}
+}
+
+extension Defaulted: Codable where T: Codable {}
