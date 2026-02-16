3071f2498cd545c850869982964b45e67141321d
Rhys Morgan <rhysm@me.com>
2026-02-16 11:42:16 +0000

    Corrects the version of Xcode being used

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -33,5 +33,6 @@ let package = Package(
             name: "PokemonKitTests",
             dependencies: ["PokemonKit"]
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
